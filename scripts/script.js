06dbc5a98897ea98ade2d2ff45a2db5a151c83e7
Gwendal <ganquetil62@gmail.com>
2022-10-27 15:38:05 +0200

    premier commit

---
+++ scripts/script.js
@@ -0,0 +1,78 @@
+const phaserConfig = {
+    type: Phaser.AUTO,
+    parent: "game",
+    width: 900,
+    height: 600,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: {y:0},
+            debug: false,
+        }
+
+    },
+    scene: {
+        init : init,
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+const game = new Phaser.Game(phaserConfig);
+
+var backgroundSand;
+
+function init(){
+
+}
+
+function preload(){
+    this.load.image("backgroundSand", "/img/sable.png")
+    this.load.spritesheet('hero','/img/hero.png',{frameWidth: 24, frameHeight : 25})
+}
+
+function create(){
+    backgroundSand = this.add.tileSprite(450,200, 900,1630, 'backgroundSand')
+    player = this.physics.add.image(450,500,'hero').setScale(5);
+    player.setCollideWorldBounds(true);
+    cursors = this.input.keyboard.createCursorKeys()
+    this.anims.create({
+        key: 'left',
+        frames: this.anims.generateFrameNumbers('hero', {start: 0, end: 3}),
+        frameRate: 10,
+        repeat: -1,
+    });
+    
+    this.anims.create({
+        key: 'turn',
+        frames: [{key : 'hero', frame : 4}],
+        frameRate: 20,
+    });
+    
+    this.anims.create({
+        key: 'right',
+        frames: this.anims.generateFrameNumbers('hero', {start: 5, end: 8}),
+        frameRate: 10,
+        repeat: -1
+    });
+}
+
+function update(){
+    backgroundSand.tilePositionY -= 0.5;
+    if (cursors.left.isDown){
+        player.setVelocityX(-160);
+        player.anims.play('left', true)
+    }else if(cursors.right.isDown){
+        player.setVelocityX(160);
+        player.anims.play('right', true)
+    }else{
+        player.setVelocityX(0)
+        player.anims.play('turn');
+    }
+    if (cursors.up.isDown && player.body.touching.down)
+    {
+        player.setVelocityY(-330)
+    }
+
+}
