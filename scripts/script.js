475aaa75dbb68b3fb8353bb9aca0ef17fb5a8258
Gwendal <ganquetil62@gmail.com>
2022-10-27 16:12:21 +0200

    mise en page

--- scripts/script.js
+++ scripts/script.js
@@ -32,17 +32,25 @@ function preload(){
     this.load.spritesheet('hero','/img/hero.png',{frameWidth: 24, frameHeight : 25})
 }
 
+
+
 function create(){
     backgroundSand = this.add.tileSprite(450,200, 900,1630, 'backgroundSand')
     player = this.physics.add.sprite(450,500,'hero').setScale(5);
     player.setCollideWorldBounds(true);
     cursors = this.input.keyboard.createCursorKeys()
 
+}
+
+
+
+
 function update(){
     backgroundSand.tilePositionY -= 0.5;
     player.setVelocityX(0);
     if (cursors.left.isDown){
         player.setVelocityX(-90);
+
 
     }else if(cursors.right.isDown){
         player.setVelocityX(90);
