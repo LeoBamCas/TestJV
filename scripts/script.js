9a4c615d172753cf3c019f81641e8647b8486b17
Gwendal <ganquetil62@gmail.com>
2022-10-28 10:00:59 +0200

    animation mauvaise spritesheet

--- scripts/script.js
+++ scripts/script.js
@@ -29,19 +29,44 @@ function init(){
 
 function preload(){
     this.load.image("backgroundSand", "/img/sable.png")
-    this.load.spritesheet('hero','/img/hero.png',{frameWidth: 24, frameHeight : 25})
+    this.load.spritesheet('hero','/img/hero.png',{frameWidth: 21, frameHeight : 25})
 }
 
 
 
 function create(){
     backgroundSand = this.add.tileSprite(450,200, 900,1630, 'backgroundSand')
-    player = this.physics.add.sprite(450,500,'hero').setScale(5);
+    player = this.physics.add.sprite(450,350,'hero').setScale(5);
+    //player.frame = 3;
     player.setCollideWorldBounds(true);
     cursors = this.input.keyboard.createCursorKeys()
 
+    this.anims.create({
+        key: 'left',
+        frames: this.anims.generateFrameNumbers('hero', {start: 4, end: 5}),
+        frameRate: 10,
+    })
+    this.anims.create({
+        key: 'right',
+        frames: this.anims.generateFrameNumbers('hero', {start: 2, end: 3}),
+        frameRate: 10
+    })
+    this.anims.create({
+        key: 'up',
+        frames: this.anims.generateFrameNumbers('hero', {start : 6, end: 7}),
+        frameRate: 15
+    })
+    this.anims.create({
+        key: 'down',
+        frames: this.anims.generateFrameNumbers('hero', {start : 0, end: 1}),
+        frameRate: 10
+    })
+    this.anims.create({
+        key: 'still',
+        frames: [{key : 'hero', frame: 8}],
+        frameRate: 20
+    })
 }
-
 
 
 
@@ -50,16 +75,22 @@ function update(){
     player.setVelocityX(0);
     if (cursors.left.isDown){
         player.setVelocityX(-90);
-
+        player.anims.play('left', true)
 
     }else if(cursors.right.isDown){
         player.setVelocityX(90);
+        player.anims.play('right', true)
 
-    }else{
-        player.setVelocityY(60)
-    }
-    if (cursors.up.isDown ){
-        player.setVelocityY(-90)
+     } else if (cursors.up.isDown ){
+            player.setVelocityY(-90)
+            player.anims.play('up', true)
+        }else if(cursors.down.isDown){
+            player.setVelocityY(120)
+            player.anims.play('down', true)
+        }
+    else{
+        player.setVelocityY(30)
+        player.anims.play('still', true)
     }
 
 }
