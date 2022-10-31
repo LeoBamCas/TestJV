a502823fc141a6dc1c267b19de07ff4e5f9d5f30
Gwendal <ganquetil62@gmail.com>
2022-10-31 15:08:06 +0100

    victoire

--- scripts/script.js
+++ scripts/script.js
@@ -43,6 +43,7 @@ function create(){
     player = this.physics.add.sprite(450,350,'hero').setScale(1);
     player.setCollideWorldBounds(true);
     bushes = this.physics.add.group();
+    ufo = this.physics.add.image(450,-100,'spaceship').setScale(0.5)
    
     bones = this.physics.add.group();
     cursors = this.input.keyboard.createCursorKeys()
@@ -101,10 +102,6 @@ function create(){
         score +=1
     }
     
-    if (score >= 3){
-        timedEvent = this.time.delayedCall(3000, setUfo, [], this)
-        this.physics.add.collider(ufo, player, flyAway, null, this)
-    }
 }
 
 
@@ -115,62 +112,66 @@ function update(){
     if (cursors.left.isDown){
         player.setVelocityX(-90);
         player.anims.play('left', true)
-
+        
     }else if(cursors.right.isDown){
         player.setVelocityX(90);
         player.anims.play('right', true)
-
-     } else if (cursors.up.isDown ){
-            player.setVelocityY(-90)
-            player.anims.play('up', true)
-        }else if(cursors.down.isDown){
-            player.setVelocityY(120)
+        
+    } else if (cursors.up.isDown ){
+        player.setVelocityY(-90)
+        player.anims.play('up', true)
+    }else if(cursors.down.isDown){
+        player.setVelocityY(120)
             player.anims.play('down', true)
         }
-    else{
-        player.setVelocityY(30)
-        player.anims.play('still', true)
+        else{
+            player.setVelocityY(30)
+            player.anims.play('still', true)
+        }
+        flames.children.iterate(function(child){
+            child.anims.play('burn', true)
+        })
+        if (score >= 15){
+            timedEvent = this.time.delayedCall(3000, setUfo, [], this)
+        }
+        this.physics.add.collider(ufo, player, flyAway, null, this)
+        
     }
-    flames.children.iterate(function(child){
-        child.anims.play('burn', true)
-    })
-
-}
-
-function onEvent(){
-    if(gameOver === false && score < 15){
-    var bush = bushes.create(Math.floor(Math.random()*900),0, 'bush').setScale(0.5);
-    bush.setVelocityY(30);
-    bush.setPushable(false);
-
-        timedEvent = this.time.delayedCall(6000, onEvent,[],this)
-    }
-}
-function setBone(){
-    if(gameOver === false && score<15){
-        var bone = bones.create(Math.floor(Math.random()*900),0, 'bone').setScale(0.5);
-        bone.setVelocityY(30);
-        bone.setPushable(false);
     
+    function onEvent(){
+        if(gameOver === false && score < 15){
+            var bush = bushes.create(Math.floor(Math.random()*900),-50, 'bush').setScale(0.5);
+            bush.setVelocityY(30);
+            bush.setPushable(false);
+            
+            timedEvent = this.time.delayedCall(6000, onEvent,[],this)
+        }
+    }
+    function setBone(){
+        if(gameOver === false && score<15){
+            var bone = bones.create(Math.floor(Math.random()*900),-50, 'bone').setScale(0.5);
+            bone.setVelocityY(30);
+            bone.setPushable(false);
+            
             timedEvent = this.time.delayedCall(6000, setBone,[],this)
         }
-}
-
-function getFried(){
-    player.setTint(0xff0000);
-    this.physics.pause()
-    player.anims.play('burn', true)
-    gameOver = true;
-
-}
-
-function setUfo(){
-    ufo = this.physics.add.image(450,0,'spaceship')
-    ufo.setVelocityY(30)
-}
-
-function flyAway(){
-    player.setTint(0x71e842);
+    }
+    
+    function getFried(){
+        player.setTint(0xff0000);
+        this.physics.pause()
+        player.anims.play('burn', true)
+        gameOver = true;
+        
+    }
+    
+    function setUfo(){
+        
+        ufo.setVelocityY(30)
+    }
+    
+    function flyAway(){
+        player.setTint(0x71e842);
     this.physics.pause()
     player.anims.play('burn', true)
     gameOver = true;
